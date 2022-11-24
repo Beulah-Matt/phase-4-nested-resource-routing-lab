19d6ed992f8453732a44daa69ffc60fc75d5ba7b
Beulah Mathenge <beulahmathenge@gmail.com>
2022-11-24 16:12:21 +0300

    Completed the assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show] do
+    #resource for items, a user has many items
+    resources :items, only: [:index, :show, :create]
+  end
   resources :items, only: [:index]
-  resources :users, only: [:show]
 end
